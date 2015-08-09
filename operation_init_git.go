bb019dc643b8ff952f3cf77f608e1bb66177b79c
James Nesbitt <james.nesbitt@wunderkraut.com>
2015-08-09 13:53:11 +0300

    make init git method more verbose, connected os.Stdin for passphrases

--- operation_init_git.go
+++ operation_init_git.go
@@ -15,19 +15,23 @@ func (operation *Operation_Init) Init_Git_Run(flags []string) (bool, map[string]
 	url := flags[0]
 	path := operation.root
 
-	cmd := exec.Command("git", "clone", url, path)
+	cmd := exec.Command("git", "clone", "--progress", url, path)
 	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
+	cmd.Stdout = operation.log
+	cmd.Stderr = operation.log
 
 	operation.log.Message("Clone remote repository to local project folder ["+url+"]")
 	err := cmd.Run()
+
 	if err!=nil {
 		operation.log.Error("Failed to clone the remote repository ["+url+"] => "+err.Error())
+		return false, map[string]string{}
 	} else {
+
 		operation.log.Message("Cloned remote repository to local project folder")
+		return true, map[string]string{
+			".coach/CREATEDFROM.md":  `THIS PROJECT WAS CREATED FROM GIT`,
+		}
 	}
 
-	return true, map[string]string{
-		".coach/CREATEDFROM.md":  `THIS PROJECT WAS CREATED FROM GIT`,
-	}
 }
