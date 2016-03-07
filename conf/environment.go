5e993e6dfc716d3bcacfbe9511855f98c7cd4009
James Nesbitt <james.nesbitt@wunderkraut.com>
2016-03-07 16:11:51 +0200

    fix bad path used for environment path

--- conf/environment.go
+++ conf/environment.go
@@ -2,6 +2,7 @@ package conf
 
 import (
 	"strconv"
+	"path"
 
 	"github.com/james-nesbitt/coach/log"
 )
@@ -20,6 +21,8 @@ func (project *Project) from_EnvironmentsPath(logger log.Log) {
 	for _, yamlEnvironmentPath := range project.Paths.GetConfSubPaths(COACH_CONF_ENVIRONMENTS_SUBPATH) {
 		logger.Debug(log.VERBOSITY_DEBUG_STAAAP, "Looking for Environment subpath: "+yamlEnvironmentPath)
 
+		yamlEnvironmentPath = path.Join(yamlEnvironmentPath, project.Environment)
+
 		if project.CheckFileExists(yamlEnvironmentPath) {
 
 			logger.Debug(log.VERBOSITY_DEBUG, "ADDING Environment subpath: "+yamlEnvironmentPath)
@@ -28,7 +31,7 @@ func (project *Project) from_EnvironmentsPath(logger log.Log) {
 			envid := COACH_CONF_ENVIRONMENTS_SUBPATH+"-"+strconv.Itoa(environmentIncrement)
 
 			project.SetPath(envid, yamlEnvironmentPath, true)
-			project.setConfPath("project-coach")
+			project.setConfPath(envid)
 
 		}
 	}
