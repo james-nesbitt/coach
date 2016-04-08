cce813cc67dd130595e06717e74988465b8448d5
James Nesbitt <james.nesbitt@wunderkraut.com>
2016-04-08 17:48:52 +0300

    init-generate cleanup, and gofmt

--- initialize/generate.go
+++ initialize/generate.go
@@ -2,6 +2,8 @@ package initialize
 
 import (
 	"io"
+	"os"
+	"path"
 
 	"github.com/james-nesbitt/coach/log"
 )
@@ -12,14 +14,14 @@ func Init_Generate(logger log.Log, handler string, path string, skip []string, o
 	var generator Generator
 	switch handler {
 	case "yaml":
-		generator = Generator(&YMLInitGenerator{})
+		generator = Generator(&YMLInitGenerator{logger: logger, output: output})
 	default:
-		logger.Error("Unknown init generator (handler) "+handler)
-		return false;
+		logger.Error("Unknown init generator (handler) " + handler)
+		return false
 	}
 
-	success := generator.Generate(logger, path, skip, output)
-	if (success) {
+	success := Init_Generate_Recursive(logger, generator, path, "", skip)
+	if success {
 		logger.Message("FINISHED GENERATING YML INIT")
 	} else {
 		logger.Error("ERROR OCCURRED GENERATING YML INIT")
@@ -27,8 +29,79 @@ func Init_Generate(logger log.Log, handler string, path string, skip []string, o
 	return success
 }
 
+func Init_Generate_Recursive(logger log.Log, generator Generator, sourceRootPath string, sourcePath string, skip []string) bool {
+	fullPath := sourceRootPath
+
+	if sourcePath != "" {
+		fullPath = path.Join(fullPath, sourcePath)
+	}
+
+	for _, skipEach := range skip {
+		if skipEach == sourcePath {
+			logger.Info("Skipping marked skip file :" + sourcePath)
+			return true
+		}
+	}
+
+	// get properties of source dir
+	info, err := os.Stat(fullPath)
+	if err != nil {
+		// @TODO do something log : source doesn't exist
+		logger.Warning("File does not exist :" + fullPath)
+		return false
+	}
+
+	mode := info.Mode()
+	if mode.IsDir() {
+
+		// check for GIT folder
+		if _, err := os.Open(path.Join(fullPath, ".git")); err == nil {
+			if generator.generateGit(fullPath, sourcePath) {
+				logger.Info("Generated git file: " + sourcePath)
+				return true
+			} else {
+				logger.Warning("Failed to generate git file: " + sourcePath)
+			}
+		}
+
+		directory, _ := os.Open(fullPath)
+		defer directory.Close()
+		objects, err := directory.Readdir(-1)
+
+		if err != nil {
+			// @TODO do something log : source doesn't exist
+			logger.Warning("Could not open directory")
+			return false
+		}
+
+		for _, obj := range objects {
+
+			//childSourcePath := source + "/" + obj.Name()
+			childSourcePath := path.Join(sourcePath, obj.Name())
+			if !Init_Generate_Recursive(logger, generator, sourceRootPath, childSourcePath, skip) {
+				logger.Warning("Resursive generate failed")
+			}
+
+		}
+
+	} else if mode.IsRegular() {
+		// add file copy
+		if generator.generateSingleFile(fullPath, sourcePath) {
+			logger.Info("Generated file (recursively): " + sourcePath)
+			return true
+		} else {
+			logger.Warning("Failed to generate file: " + sourcePath)
+			return false
+		}
+		return true
+	} else {
+		logger.Warning("Skipped generation non-regular file: " + sourcePath)
+	}
+
+	return true
+}
+
 type Generator interface {
-
-	Generate(logger log.Log, path string, skip []string, output io.Writer) bool
-
+	generateSingleFile(fullPath string, sourcePath string) bool
+	generateGit(fullPath string, sourcePath string) bool
 }
--- initialize/yaml_generate.go
+++ initialize/yaml_generate.go
@@ -1,8 +1,8 @@
 package initialize
 
 import (
-	"io"
 	"bufio"
+	"io"
 	"os"
 	"path"
 	"strings"
@@ -14,81 +14,6 @@ type YMLInitGenerator struct {
 	output io.Writer
 	logger log.Log
 }
-func (generator *YMLInitGenerator) Generate(logger log.Log, path string, skip []string, output io.Writer) bool {
-	generator.logger = logger
-	generator.output = output
-
-	return generator.generateFileRecursive(path, "", skip)
-}
-func (generator *YMLInitGenerator)  generateFileRecursive(sourceRootPath string, sourcePath string, skip []string) bool {
-	fullPath := sourceRootPath
-	logger := generator.logger
-
-	if sourcePath != "" {
-		fullPath = path.Join(fullPath, sourcePath)
-	}
-
-	for _, skipEach := range skip {
-		if skipEach == sourcePath {
-			logger.Info("Skipping marked skip file :"+sourcePath)
-			return true
-		}
-	}
-
-	// get properties of source dir
-	info, err := os.Stat(fullPath)
-	if err != nil {
-		// @TODO do something log : source doesn't exist
-		logger.Warning("File does not exist :" + fullPath)
-		return false
-	}
-
-	mode := info.Mode()
-	if mode.IsDir() {
-
-		// check for GIT folder
-		if _, err := os.Open(path.Join(fullPath, ".git")); err == nil {
-			if generator.generateGit(fullPath, sourcePath) {
-				logger.Info("Generated git file: " + sourcePath)
-				return true
-			} else {
-				logger.Warning("Failed to generate git file: " + sourcePath)
-			}			
-		}
-
-		directory, _ := os.Open(fullPath)
-		defer directory.Close()
-		objects, err := directory.Readdir(-1)
-
-		if err != nil {
-			// @TODO do something log : source doesn't exist
-			logger.Warning("Could not open directory")
-			return false
-		}
-
-		for _, obj := range objects {
-
-			//childSourcePath := source + "/" + obj.Name()
-			childSourcePath := path.Join(sourcePath, obj.Name())
-			if !generator.generateFileRecursive(sourceRootPath, childSourcePath, skip) {
-				logger.Warning("Resursive generate failed")
-			}
-
-		}
-
-	} else {
-		// add file copy
-		if generator.generateSingleFile(fullPath, sourcePath) {
-			logger.Info("Generated file (recursively): " + sourcePath)
-			return true
-		} else {
-			logger.Warning("Failed to generate file: " + sourcePath)
-			return false
-		}
-		return true
-	}
-	return true
-}
 
 func (generator *YMLInitGenerator) generateSingleFile(fullPath string, sourcePath string) bool {
 	singleFile, _ := os.Open(fullPath)
@@ -96,7 +21,7 @@ func (generator *YMLInitGenerator) generateSingleFile(fullPath string, sourcePat
 
 	generator.logger.Debug(log.VERBOSITY_DEBUG_LOTS, "GENERATE SINGLE FILE: ", singleFile.Name())
 	generator.output.Write([]byte("- Type: File\n"))
-	generator.output.Write([]byte("  Path: "+sourcePath+"\n"))
+	generator.output.Write([]byte("  Path: " + sourcePath + "\n"))
 	generator.output.Write([]byte("  Contents: |\n"))
 
 	r := bufio.NewReader(singleFile)
@@ -108,7 +33,7 @@ func (generator *YMLInitGenerator) generateSingleFile(fullPath string, sourcePat
 			generator.logger.Error(err.Error())
 			return false // if you return error
 		}
-		generator.output.Write([]byte("    "+line+"\n"))
+		generator.output.Write([]byte("    " + line))
 	}
 	return true
 }
@@ -116,7 +41,7 @@ func (generator *YMLInitGenerator) generateGit(fullPath string, sourcePath strin
 
 	gitUrl := ""
 
-	if configFile, err := os.Open(path.Join(fullPath, ".git", "config")); err==nil {
+	if configFile, err := os.Open(path.Join(fullPath, ".git", "config")); err == nil {
 
 		r := bufio.NewReader(configFile)
 		for {
@@ -135,18 +60,18 @@ func (generator *YMLInitGenerator) generateGit(fullPath string, sourcePath strin
 		}
 
 	} else {
-		generator.logger.Error("Could not open .git/config in " +sourcePath)
+		generator.logger.Error("Could not open .git/config in " + sourcePath)
 		return false
 	}
 
-	if gitUrl=="" {
+	if gitUrl == "" {
 		generator.logger.Error("Could not determine GIT Url from .git/config")
 		return false
 	}
 
 	generator.logger.Debug(log.VERBOSITY_DEBUG_LOTS, "GENERATE GIT FILE: ", sourcePath)
 	generator.output.Write([]byte("- Type: GitClone\n"))
-	generator.output.Write([]byte("  Path: "+sourcePath+"\n"))
-	generator.output.Write([]byte("  Url: "+gitUrl+"\n"))
-	return true	
+	generator.output.Write([]byte("  Path: " + sourcePath + "\n"))
+	generator.output.Write([]byte("  Url: " + gitUrl + "\n"))
+	return true
 }
